60920903e3b24d8be1e84341f32b140d8305b1b2
viable-hartman <viable.hartman@gmail.com>
2017-06-06 15:44:09 -0700

    Modified underlying client_class.py METHOD and internal functions to give the end-user better control over calling their REST APIs in case they don't follow the standard CRUD model.  This allows for calls like base_url+'/some_string_or_different_format/or_different_path_entirely'.
    Added a feature to allow striping the trailing '/' from the API url if desired.
    
    Added a feature to allow passing custom HEADERS with the API calls.
    
    Modified, base functions to also allow passing a JSON body to an api call
    instead of just parameters, as some APIs take strictly JSON.

--- pythonrestclient/api/client_class.py
+++ pythonrestclient/api/client_class.py
@@ -5,62 +5,70 @@ from requests.auth import HTTPBasicAuth
 
 
 class ClientClass:
-    def __init__(self, base_url, username=None, password=None):
+    def __init__(self, base_url, username=None, password=None, custom_headers=None, wo_trailing=False):
         self.base_url = base_url
         self.username = username
         self.password = password
+        self.custom_headers = custom_headers
+        self.wo_trailing = wo_trailing
 
-    def get_resources(self, resources_name, params=None):
-        url = self._make_url('/{:s}/'.format(resources_name))
+    def get_resources(self, resources_name, params=None, url_format='/{:s}/'):
+        url = self._make_url(url_format.format(resources_name))
         response = self._make_get_request(url, params)
         self._log_response(response)
         return self.decoded_response(response)
 
-    def get(self, resources_name, resource_id):
-        url = self._make_url('/{:s}/{:d}/'.format(resources_name, int(resource_id)))
+    def get(self, resources_name, resource_id, url_format='/{:s}/{:d}/'):
+        url = self._make_url(url_format.format(resources_name, int(resource_id)))
         response = self._make_get_request(url)
         self._log_response(response)
         return self.decoded_response(response)
 
-    def post(self, resources_name, params):
-        url = self._make_url('/{:s}/'.format(resources_name))
-        response = self._make_post_request(url, params)
+    def post(self, resources_name, params=None, data=None, url_format='/{:s}/'):
+        url = self._make_url(url_format.format(resources_name))
+        response = self._make_post_request(url, params, data)
         self._log_response(response)
         return self.decoded_response(response)
 
-    def put(self, resources_name, resource_id, params):
-        url = self._make_url('/{:s}/{:d}/'.format(resources_name, int(resource_id)))
+    def put(self, resources_name, resource_id, params=None, data=None, url_format='/{:s}/{:d}/'):
+        url = self._make_url(url_format.format(resources_name, int(resource_id)))
         response = self._make_put_request(url, params)
         self._log_response(response)
         return self.decoded_response(response)
 
-    def delete(self, resources_name, resource_id):
-        url = self._make_url('/{:s}/{:d}/'.format(resources_name, int(resource_id)))
+    def delete(self, resources_name, resource_id, url_format='/{:s}/{:d}/'):
+        url = self._make_url(url_format.format(resources_name, int(resource_id)))
         response = self._make_delete_request(url)
         self._log_response(response)
         return self.decoded_response(response)
 
     def _make_url(self, path):
-        return self.base_url + path
+        url = self.base_url + path
+        if self.wo_trailing and url[-1] == '/':
+            return url[:-1]
+        return url
 
     def _make_get_request(self, url, params=None):
         return self._make_request('get', url, params)
 
-    def _make_post_request(self, url, params):
-        return self._make_request('post', url, params)
+    def _make_post_request(self, url, params=None, data=None):
+        return self._make_request('post', url, params, data)
 
-    def _make_put_request(self, url, params):
-        return self._make_request('put', url, params)
+    def _make_put_request(self, url, params=None, data=None):
+        return self._make_request('put', url, params, data)
 
     def _make_delete_request(self, url):
         return self._make_request('delete', url, None)
 
-    def _make_request(self, method, url, params):
+    def _make_request(self, method, url, params=None, data=None):
         kwargs = {}
         if self.username is not None:
             kwargs['auth'] = HTTPBasicAuth(self.username, self.password)
-        self._log_request(method, url, params)
-        response = requests.request(method, url, params=params, **kwargs)
+        self._log_request(method, url, params, data, self.custom_headers)
+        if data:
+            response = requests.request(method, url, headers=self.custom_headers, params=params, data=json.dumps(data), **kwargs)
+        else:
+            response = requests.request(method, url, headers=self.custom_headers, params=params, **kwargs)
         self._check_response(response)
         return response
 
@@ -78,9 +86,13 @@ class ClientClass:
     # TODO: print log to file
 
     @staticmethod
-    def _log_request(method, url, params):
-        print('\nMaking {:s} request to url="{:s}" with data:'.format(method, url))
+    def _log_request(method, url, params, data, headers):
+        print('\nMaking {:s} request to url="{:s}" with headers:'.format(method, url))
+        print(headers)
+        print(' and params:')
         print(params)
+        print(' and data:')
+        print(data)
 
     @staticmethod
     def _log_response(response):
--- pythonrestclient/service_factory.py
+++ pythonrestclient/service_factory.py
@@ -5,5 +5,5 @@ class ServiceFactory(object):
     api_client = None
 
     @classmethod
-    def init_api_client(cls, base_url, username=None, password=None):
-        cls.api_client = ClientClass(base_url, username, password)
+    def init_api_client(cls, base_url, username=None, password=None, custom_headers=None, wo_trailing=False):
+        cls.api_client = ClientClass(base_url, username, password, custom_headers, wo_trailing)
